bd5fd6476ffb86027973379aeafa46d8423242d0
sooof <sooof@me.com>
2022-01-13 18:27:06 -0800

    use dotenv

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const server = require('./api/server.js');
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+require('dotenv').config() //npm i dotenv
+const server = require('./api/server.js');
+const port = process.env.PORT;
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+// node index.js 4000
