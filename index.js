570293f5d6d6f6a9cafafa4d50d321993d584f3a
sooof <sooof@me.com>
2022-01-15 21:23:25 -0800

    use path

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
+require('dotenv').config()
+const path = require('path')
 
- console.log(process.argv)
- const cohort1 = process.argv[2]
- const user = process.env.USER
- const shell = process.env.SHELL
+//  console.log(process.argv)
+//  const cohort1 = process.argv[2]
+//  const user = process.env.USER
+//  const shell = process.env.SHELL
 
-if(cohort1 === 'web-49'){
-  console.log(`${cohort1}  is the best forevert!`)
-}else{
-  console.log(`boooh ${cohort1}`)
-}
+// if(cohort1 === 'web-49'){
+//   console.log(`${cohort1}  is the best forevert!`)
+// }else{
+//   console.log(`boooh ${cohort1}`)
+// }
 
-console.log(`the user is ${user}`)
-console.log(`the shell is ${shell}`)
+// console.log(`the user is ${user}`)
+// console.log(`the shell is ${shell}`)
 
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'cloent/build')))
+
 app.get('/hello', (req, res)=>{
   res.json({message: 'hey there'})
 })
-
-const port =9000
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'cloent/build', 'index.html'))
+})
+const port = process.env.PORT || 9000
+console.log(port)
 app.listen(port, ()=>{
-  console.log(`listening on port 9000`)
+  console.log(`listening on port ${port}`)
 })
