04eae2a5edc4e929203793ed4c2f491e8bf15777
sooof <sooof@me.com>
2022-01-15 21:13:14 -0800

    use path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  console.log(process.argv)
  const cohort1 = process.argv[2]
  const user = process.env.USER
+ const shell = process.env.SHELL
 
 if(cohort1 === 'web-49'){
   console.log(`${cohort1}  is the best forevert!`)
@@ -10,3 +11,16 @@ if(cohort1 === 'web-49'){
 }
 
 console.log(`the user is ${user}`)
+console.log(`the shell is ${shell}`)
+
+const express = require('express')
+const app = express()
+
+app.get('/hello', (req, res)=>{
+  res.json({message: 'hey there'})
+})
+
+const port =9000
+app.listen(port, ()=>{
+  console.log(`listening on port 9000`)
+})
